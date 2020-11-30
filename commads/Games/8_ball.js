288d5b04c9461014ca04af6b2ab027fae50313a7
Sensei <71566572+Sensei-911@users.noreply.github.com>
2020-11-30 22:25:52 +0300

    Create 8_ball.js

---
+++ commads/Games/8_ball.js
@@ -0,0 +1,37 @@
+       const Discord = require('discord.js');
+       var cevaplar = [
+       "Kaynaklarım eveti gösteriyor",
+       "Sonra tekrar sor",
+       "Evet",
+       "Hayır",
+       "Kesinlikle",
+       "Bende öyle düşünüyorum",
+       "Sen ne dersen o",
+       "Şüphesiz",
+       "Şimdi tahmin edemem",
+       "İşaretler eveti gösteriyor",
+       "Bilemedim",
+       "Yup",
+       "Bilmem",
+       "Doğru diyorsun",
+       "Hai onii-san",
+       "Sadece bugün için",
+       "Belki",
+       "Kimseye söyleme ama hayır ( ͡° ͜ʖ ͡°)",
+       "Bunun cevabını yarın söylerim",
+       "Cevaplamaya üşendim şimdi",
+       "Sen dersinde olmazmı",
+       "Tabi",
+]
+       
+       module.exports = {
+           name: '8ball',
+           aliases: ["8b"],
+           description: 'Rastgele cevap verir',
+           execute(client, message, args) {
+            let sekizball_mesaj = args.slice(0).join(' ');
+            if (sekizball_mesaj.length < 1) return message.channel.send(`**${message.author.username}** Bir soru sormalısın.`)
+               var cevap = cevaplar[Math.floor(Math.random()*(cevaplar.length))]
+               message.channel.send(`🎱 **| ${message.author.username} sordu :** ${sekizball_mesaj}
+<:blank:776723076574347264> **| Cevap :** ${cevap}`)
+}}
