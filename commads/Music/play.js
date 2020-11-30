7c04e531158a624523a3905a61ece58c99129dc1
Sensei <71566572+Sensei-911@users.noreply.github.com>
2020-11-30 22:27:37 +0300

    Create play.js

---
+++ commads/Music/play.js
@@ -0,0 +1,112 @@
+const { play } = require("../../utils/play");
+const { YOUTUBE_API_KEY, SOUNDCLOUD_CLIENT_ID } = require("../../config.json");
+const ytdl = require("ytdl-core");
+const YouTubeAPI = require("simple-youtube-api");
+const youtube = new YouTubeAPI(YOUTUBE_API_KEY);
+const scdl = require("soundcloud-downloader");
+
+module.exports = {
+  name: "play",
+  cooldown: 3,
+  aliases: ["p"],
+  description: "Müzik oynatır",
+  async execute(client, message, args) {
+    const { channel } = message.member.voice;
+
+    const serverQueue = message.client.queue.get(message.guild.id);
+    if (!channel) return message.reply("Önce bir sesli kanala girmelisin!").catch(console.error);
+    if (serverQueue && channel !== message.guild.me.voice.channel)
+      return message.reply(`${message.client.user} ile aynı kanalda olmalısınız`).catch(console.error);
+
+    if (!args.length)
+      return message.reply(`Listede şarkı yok`).catch(console.error);
+  
+    const permissions = channel.permissionsFor(message.client.user);
+    if (!permissions.has("CONNECT"))
+      return message.reply("Ses kanalına bağlanamıyorum, uygun izinlere sahip olduğumdan emin olun!");
+    if (!permissions.has("SPEAK"))
+      return message.reply("Bu ses kanalında konuşamıyorum, uygun izinlere sahip olduğumdan emin olun!");
+
+    const search = args.join(" ");
+    const videoPattern = /^(https?:\/\/)?(www\.)?(m\.)?(youtube\.com|youtu\.?be)\/.+$/gi;
+    const playlistPattern = /^.*(list=)([^#\&\?]*).*/gi;
+    const scRegex = /^https?:\/\/(soundcloud\.com)\/(.*)$/;
+    const url = args[0];
+    const urlValid = videoPattern.test(args[0]);
+
+    // Start the playlist if playlist url was provided
+    if (!videoPattern.test(args[0]) && playlistPattern.test(args[0])) {
+      return message.client.commands.get("playlist").execute(message, args);
+    } /*else if (scdl.isValidUrl(url) && url.includes("/sets/")) {
+      return message.client.commands.get("playlist").execute(message, args);
+    }*/
+
+    const queueConstruct = {
+      textChannel: message.channel,
+      channel,
+      connection: null,
+      songs: [],
+      loop: false,
+      volume: 100,
+      playing: true
+    };
+
+    let songInfo = null;
+    let song = null;
+
+    if (urlValid) {
+      try {
+        songInfo = await ytdl.getInfo(url);
+        song = {
+          title: songInfo.videoDetails.title,
+          url: songInfo.videoDetails.video_url,
+          duration: songInfo.videoDetails.lengthSeconds
+        };
+      } catch (error) {
+        console.error(error);
+        return message.reply(error.message).catch(console.error);
+      }
+    } else if (scRegex.test(url)) {
+      try {
+        const trackInfo = await scdl.getInfo(url, SOUNDCLOUD_CLIENT_ID);
+        song = {
+          title: trackInfo.title,
+          url: trackInfo.permalink_url,
+          duration: Math.ceil(trackInfo.duration / 1000)
+        };
+      } catch (error) {
+        if (error.statusCode === 404)
+          return message.reply("Bu Soundcloud parçası bulunamadı.").catch(console.error);
+        return message.reply("Bu Soundcloud parçası çalınırken bir hata oluştu.").catch(console.error);
+      }
+    } else {
+      try {
+        const results = await youtube.searchVideos(search, 1);
+        songInfo = await ytdl.getInfo(results[0].url);
+        song = {
+          title: songInfo.videoDetails.title,
+          url: songInfo.videoDetails.video_url,
+          duration: songInfo.videoDetails.lengthSeconds
+        };
+      } catch (error) {
+        console.error(error);
+        return message.reply("Bu başlıkla eşleşen bir video bulunamadı").catch(console.error);
+      }}
+
+    if (serverQueue) {
+      serverQueue.songs.push(song);
+      return serverQueue.textChannel.send(`✅ **${song.title}** ${message.author} tarafından listeye eklendi`).catch(console.error)}
+
+    queueConstruct.songs.push(song);
+    message.client.queue.set(message.guild.id, queueConstruct);
+
+    try {
+      queueConstruct.connection = await channel.join();
+      await queueConstruct.connection.voice.setSelfDeaf(true);
+      play(queueConstruct.songs[0], message);
+    } catch (error) {
+      console.error(error);
+      message.client.queue.delete(message.guild.id);
+      await channel.leave();
+      return message.channel.send(`Kanala katılamadım`).catch(console.error);
+    }}};
