66e46753cd2fbe950775f84905c84dea3a79b961
Sensei <71566572+Sensei-911@users.noreply.github.com>
2020-11-30 22:27:03 +0300

    Create ban.js

---
+++ commads/Moderation/ban.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+       
+          module.exports = {
+            name: "ban",
+            description: "Etiketlenen kişiyi banlayan komut",
+            execute(client, message, args) {
+              let neden = args.slice(0).join(" ")
+          
+          if(!neden) reason = `${message.author.username} tarafından banlandı.`
+          if(!message.member.hasPermission("BAN_MEMBERS")) return
+          if(!args[0]) return message.channel.send('Lütfen bir kullanıcı belirtin.');
+          var user = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
+            
+          if (user) {
+              const member = message.guild.member(user);
+          if(member.id === message.author.id) return message.channel.send('Kendini banlayamazsın!');
+          if (member) {
+            if(member.hasPermission('ADMINISTRATOR')) return message.reply('Yetkilileri banlayamam.');
+                member.ban({
+                    reason: neden}).then(() => {
+              message.channel.send(`**${user.tag}** Başarıyla banlandı`)})
+                    .catch(err => {
+              message.channel.send('Kullanıcıyı banlayamadım.');
+                    console.error(err);
+                  });
+              } else {
+              message.channel.send("Kullanıcı bu sunucuda değil!");
+              }
+    
+}}}
