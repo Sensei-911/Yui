4bf4a3f546f77f51b757233594dc2fba7b07cd6a
Sensei <71566572+Sensei-911@users.noreply.github.com>
2020-12-01 04:51:18 +0300

    Update chika.js

--- chika.js
+++ chika.js
@@ -1,10 +1,9 @@
 /*
  * Chika Bot for Discord
- * Copyright (C) 2019 Kemal
- * This software is licensed under MIT
+ * Copyright (C) 2020 Kemal H.
+ * This software is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
  * For more information, see README.md and LICENSE
-*/
-
+  */
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 const { Client, Collection } = require("discord.js");
 const { readdirSync } = require("fs");
