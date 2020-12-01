525543d84f8828a4675cb015f0c0ec2498bcf0b0
Sensei <71566572+Sensei-911@users.noreply.github.com>
2020-12-01 04:54:55 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@
  * Copyright (C) 2020 Kemal H.
  * This software is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
  * For more information, see README.md and LICENSE
-*/ 
+  */
 /*
 client.user.setStatus("idle")
 dnd,idle,online,ofline
