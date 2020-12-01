32f9e45c130f6a9cadf20d947560ebd7851ab3c8
Sensei <71566572+Sensei-911@users.noreply.github.com>
2020-12-01 05:07:22 +0300

    Update calculate.js

--- commads/Information/calculate.js
+++ commads/Information/calculate.js
@@ -1,3 +1,9 @@
+/*
+ * Chika Bot for Discord
+ * Copyright (C) 2020 Kemal H.
+ * This software is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ * For more information, see README.md and LICENSE
+  */
 const { calculator } = require('../../functions')
 const math = require('math.js');
 const Discord = require('discord.js');
