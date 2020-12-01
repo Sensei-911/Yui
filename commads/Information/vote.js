a10467ccd476f5ddaaeb9b1f72b2e065d38ff138
Sensei <71566572+Sensei-911@users.noreply.github.com>
2020-12-01 05:09:53 +0300

    Update vote.js

--- commads/Information/vote.js
+++ commads/Information/vote.js
@@ -1,3 +1,9 @@
+/*
+ * Chika Bot for Discord
+ * Copyright (C) 2020 Kemal H.
+ * This software is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ * For more information, see README.md and LICENSE
+  */
 const { votelink } = require("../../config.json");
 
 module.exports = {
