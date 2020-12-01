faf6fb81484fe68e8d2c16d1c797125d7d8b55e6
Sensei <71566572+Sensei-911@users.noreply.github.com>
2020-12-01 05:07:10 +0300

    Update avatar.js

--- commads/Information/avatar.js
+++ commads/Information/avatar.js
@@ -1,3 +1,9 @@
+/*
+ * Chika Bot for Discord
+ * Copyright (C) 2020 Kemal H.
+ * This software is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ * For more information, see README.md and LICENSE
+  */
 module.exports = {
     name: 'avatar',
     description: 'Etiketlenen kişinin avatarını veren komut',
