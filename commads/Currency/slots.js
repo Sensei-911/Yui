a9f03f95c05ef4a61963a25ebef62eec081176e8
Sensei <71566572+Sensei-911@users.noreply.github.com>
2020-12-01 05:00:58 +0300

    Update slots.js

--- commads/Currency/slots.js
+++ commads/Currency/slots.js
@@ -1,3 +1,9 @@
+/*
+ * Chika Bot for Discord
+ * Copyright (C) 2020 Kemal H.
+ * This software is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ * For more information, see README.md and LICENSE
+  */
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 const Discord = require('discord.js');
 const moving = '<a:slots:780710595900211210>'
