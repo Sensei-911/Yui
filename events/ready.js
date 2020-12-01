b5629eb3cb89143e8e0cbfec84d346acbc39443d
Sensei <71566572+Sensei-911@users.noreply.github.com>
2020-12-01 04:54:07 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,10 @@
- /*
+/*
+ * Chika Bot for Discord
+ * Copyright (C) 2020 Kemal H.
+ * This software is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ * For more information, see README.md and LICENSE
+*/ 
+/*
 client.user.setStatus("idle")
 dnd,idle,online,ofline
 WATCHING,LISTENING,PLAYING
