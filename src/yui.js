d2383e3c3a625c010b6f905daf208585fbaf956b
Sensei <71566572+Sensei-911@users.noreply.github.com>
2021-03-19 00:10:23 +0300

    Delete yui.js

--- src/yui.js
+++
@@ -1,9 +0,0 @@
-const { Master: Sharder } = require('eris-sharder')
-const config = require('./secrets/config.json')
-const Master = new Sharder(`Bot ${config.token}`, '\\src\\mainClass.js', { 
-name: "Yui",
-stats: true,
-clusters: 1,
-shards: 5,
-debug: true
-});
